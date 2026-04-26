00103d3a4890f9b960e7c521a15f1b086bbe3358
Willy Vilca Huaytalla <willyvilca06@gmail.com>
2026-04-26 10:40:44 -0500

    feat: implementando servlet dashboard

---
+++ src/main/java/com/accounting/sistemacontador/controller/DashboardServlet.java
@@ -0,0 +1,36 @@
+
+package com.accounting.sistemacontador.controller;
+
+import com.accounting.sistemacontador.dao.TransactionDAO;
+import com.accounting.sistemacontador.model.Transaction;
+import com.accounting.sistemacontador.model.User;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/dashboard")
+public class DashboardServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        TransactionDAO transactionDAO = new TransactionDAO();
+
+        if (user == null) {
+            response.sendRedirect("views/auth/login.jsp");
+            return;
+        }
+        
+        //resumen de las transacciones totales del usuario(ingresos,gastos,balance)
+        double[] summary = transactionDAO.getSummaryByUser(user.getUserId());
+        request.setAttribute("ingresos", summary[0]);
+        request.setAttribute("gastos", summary[1]);
+        request.setAttribute("balance", summary[2]);
+            
+    }
+}
