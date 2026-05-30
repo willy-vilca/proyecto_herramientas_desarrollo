f9a1014ad7366851eb2c0a90ed02b24a117fd39f
Willy Vilca Huaytalla <willyvilca06@gmail.com>
2026-05-29 20:03:34 -0500

    estructura inicial springBoot

---
+++ src/main/java/SistemaContador/SistemaContadorApplication.java
@@ -0,0 +1,13 @@
+package SistemaContador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaContadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaContadorApplication.class, args);
+	}
+
+}
